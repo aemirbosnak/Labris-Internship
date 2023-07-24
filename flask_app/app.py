e00ea3cda98c8669cf0dc2ba52f7c92cabe53f9d
Emir Boşnak <emir.bosnak@ug.bilkent.edu.tr>
2023-07-24 15:14:51 +0300

    api functionality added

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,183 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import datetime
 
 app = Flask(__name__)
 
+# Sample data (You can replace this with a database in a real application)
+users = [
+    {
+        "id": 1,
+        "username": "user1",
+        "firstname": "John",
+        "middlename": "Doe",
+        "lastname": "Smith",
+        "birthdate": "1990-01-15",
+        "email": "user1@example.com",
+        "password": "user123",
+        "is_online": False,
+        "last_login": None
+    },
+    {
+        "id": 2,
+        "username": "user2",
+        "firstname": "Jane",
+        "middlename": "Lee",
+        "lastname": "Johnson",
+        "birthdate": "1985-05-20",
+        "email": "user2@example.com",
+        "password": "user456",
+        "is_online": False,
+        "last_login": None
+    }
+]
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+# Endpoint for login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    if not data or 'username' not in data or 'password' not in data:
+        return jsonify({"error": "Invalid data. 'username' and 'password' fields are required."}), 400
+
+    username = data['username']
+    password = data['password']
+
+    user = find_user_by_username(username)
+
+    # Check if the provided username exists and the password is correct
+    if user:
+        if user["password"] == password:
+            user['is_online'] = True
+            user['last_login'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            return jsonify({"message": "Login successful!"}), 200
+        else:
+            return jsonify({"error": "Invalid password. Please check your password."}), 401
+    else:
+        return jsonify({"error": "Invalid credentials. Please check your username and password."}), 401
+
+
+# Endpoint for logout
+@app.route('/logout', methods=['POST'])
+def logout():
+    data = request.get_json()
+    if not data or 'username' not in data:
+        return jsonify({"error": "Invalid data. 'username' field is required."}), 400
+
+    username = data['username']
+    user = find_user_by_username(username)
+    if user:
+        user['is_online'] = False
+        return jsonify({"message": "Logout successful!"}), 200
+    else:
+        return jsonify({"error": "User not found."}), 404
+
+
+# Endpoint for listing all users
+@app.route('/user/list', methods=['GET'])
+def list_users():
+    return jsonify(users)
+
+
+# Endpoint for creating a new user
+@app.route('/user/create', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    if not data or 'username' not in data or 'firstname' not in data or 'lastname' not in data or \
+            'birthdate' not in data or 'email' not in data or 'password' not in data:
+        return jsonify({"error": "Invalid data. 'username', 'firstname', 'lastname', 'birthdate', "
+                                 "'email', and 'password' fields are required."}), 400
+
+    username = data['username']
+    firstname = data['firstname']
+    middlename = data.get('middlename', '')  # Optional field, set to empty string if not provided
+    lastname = data['lastname']
+    birthdate = data['birthdate']
+    email = data['email']
+    password = data['password']
+
+    # Check if the username already exists
+    if find_user_by_username(username):
+        return jsonify({"error": "Username already exists. Please choose a different username."}), 409
+
+    # Generate a unique user ID (for demo purposes, not recommended for production)
+    user_id = len(users) + 1
+
+    # Create the new user dictionary
+    new_user = {
+        "id": user_id,
+        "username": username,
+        "firstname": firstname,
+        "middlename": middlename,
+        "lastname": lastname,
+        "birthdate": birthdate,
+        "email": email,
+        "password": password,
+        "is_online": False,
+        "last_login": None
+    }
+
+    users.append(new_user)
+    return jsonify(new_user), 201
+
+
+# Endpoint for deleting a user by ID
+@app.route('/user/delete/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = find_user_by_id(user_id)
+    if not user:
+        return jsonify({"error": "User not found."}), 404
+
+    users.remove(user)
+    return jsonify({"message": "User deleted successfully."}), 200
+
+
+# Endpoint for updating a user by ID
+@app.route('/user/update/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = find_user_by_id(user_id)
+    if not user:
+        return jsonify({"error": "User not found."}), 404
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"error": "Invalid data. Please provide updated user information."}), 400
+
+    user['username'] = data.get('username', user['username'])
+    user['email'] = data.get('email', user['email'])
+    user['is_online'] = data.get('is_online', user['is_online'])
+
+    return jsonify(user), 200
+
+
+# Endpoint for getting online users
+@app.route('/onlineusers', methods=['GET'])
+def get_online_users():
+    online_users = []
+    for user in users:
+        if user['is_online']:
+            online_user_info = {
+                "username": user['username'],
+                "ipaddress": request.remote_addr,
+                "last_login": user['last_login']
+            }
+            online_users.append(online_user_info)
+    return jsonify(online_users)
+
+
+# Helper function to find a user by ID
+def find_user_by_id(user_id):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+    return None
+
+
+def find_user_by_username(username):
+    for user in users:
+        if user["username"] == username:
+            return user
+    return None
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
